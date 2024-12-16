60ac4f0b4493de737650058e3a76f650a8cfa111
a99117468@gmail.com <a99117468@gmail.com>
2024-12-16 17:56:55 +0900

    modify fizzbuzz3.py

--- fizzbuzz3.py
+++ fizzbuzz3.py
@@ -1,3 +1,9 @@
 for i in range(1, 20+1):
-    print('fizz' * (i%3==0) + 'buzz' * (i%5==0)) if i%3==0 or i%5 == 0 else print(i)
-
+    if i%15 == 0:
+        print('fizzbuzz')
+    elif i%3 == 0:
+        print('fizz')
+    elif i%5 == 0:
+        print('buzz')
+    else:
+        print(i)
