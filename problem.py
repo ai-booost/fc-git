cc0d6174df4c8f3a7a326f2cf744a63b038d086f
happyhyerin333 <gpflswjd0793@gmail.com>
2024-12-16 18:37:43 +0900

    feat : Write meaning of definition

--- problem.py
+++ problem.py
@@ -1,5 +1,5 @@
-def prisoner_strategy(prisoner_number, boxes):
-    attempts = 50
+def prisoner_strategy(prisoner_number, boxes): #2번째 함수 정의
+    attempts = 50 
     current_box = prisoner_number
     for _ in range(attempts):
         if boxes[current_box -1] == prisoner_number:
